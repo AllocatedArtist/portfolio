e08b29374c8c66eddba8c0af9376166cadfc88a8
AllocatedArtist <59222318+AllocatedArtist@users.noreply.github.com>
2026-08-31 22:40:12 -0700

    io to com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from '@astrojs/sitemap';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-  site: 'https://sevenf.portfolio.io',
+  site: 'https://sevenf.portfolio.com',
   integrations: [sitemap()],
   vite: { plugins: [glsl({ compress: true })] },
 });
