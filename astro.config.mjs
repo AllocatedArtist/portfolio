36d7f51af46abec53a366b41b57e4f91ecba5cc1
AllocatedArtist <59222318+AllocatedArtist@users.noreply.github.com>
2026-08-31 22:44:23 -0700

    reverted site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from '@astrojs/sitemap';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-  site: 'https://sevenf.portfolio.com',
+  site: 'https://allocatedartist.github.io',
   integrations: [sitemap()],
   vite: { plugins: [glsl({ compress: true })] },
 });
